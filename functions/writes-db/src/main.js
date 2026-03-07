4280cbcdbe94ebb195e9eb47a032b9ad6139df57
J-Branch <jamalbranch01@gmail.com>
2026-03-07 10:09:30 -0500

    re added appwrite config

--- functions/writes-db/src/main.js
+++ functions/writes-db/src/main.js
@@ -1,4 +1,4 @@
-import { Client, Users, TablesDB } from 'node-appwrite';
+import { Client, TablesDB } from 'node-appwrite';
 
 // This Appwrite function will be executed every time your function is triggered
 export default async ({ req, res, log, error }) => {
@@ -8,22 +8,59 @@ export default async ({ req, res, log, error }) => {
     .setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT)
     .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
     .setKey(process.env.APPWRITE_API_KEY);
-  const users = new Users(client);
 
-  try {
-    const response = await users.list();
-    // Log messages and errors to the Appwrite Console
-    // These logs won't be seen by your end users
-    log(`Total users: ${response.total}`);
-  } catch(err) {
-    error("Could not list users: " + err.message);
+  const tablesdb = new TablesDB(client);
+  const DATABASE_ID = process.env.APPWRITE_DATABASE_ID;
+  const WORKOUTS_ID = process.env.APPWRITE_WORKOUTS_ID;
+  const EXERCISES_ID = process.env.APPWRITE_EXERCISES_ID;
+  const SETS_ID = process.env.APPWRITE_SETS_ID;
+
+  // try {
+  //   const response = await users.list();
+  //   // Log messages and errors to the Appwrite Console
+  //   // These logs won't be seen by your end users
+  //   log(`Total users: ${response.total}`);
+  // } catch(err) {
+  //   error("Could not list users: " + err.message);
+  // }
+
+
+
+  // // The req object contains the request data
+  // if (req.path === "/ping") {
+  //   // Use res object to respond with text(), json(), or binary()
+  //   // Don't forget to return a response!
+  //   return res.text("Pong");
+  // }
+
+  async function deleteRow(node) {
+    if (node.table === "workouts" && node.toDelete === true) {
+
+    }
   }
 
-  // The req object contains the request data
-  if (req.path === "/ping") {
-    // Use res object to respond with text(), json(), or binary()
-    // Don't forget to return a response!
-    return res.text("Pong");
+  async function updateRow(node) {
+
+  }
+
+  async function createRow(node) {
+
+  }
+
+  try {
+    const payload = JSON.parse(req.body)
+
+    // check for deletes first
+    deleteRow
+
+    // check for updates second
+
+    // check for creates
+
+
+    return res.json({sucess: true})
+  } catch (err) {
+    return res.json({sucess: false, message: err.message});
   }
 
   return res.json({
